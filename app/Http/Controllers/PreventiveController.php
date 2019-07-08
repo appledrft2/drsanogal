be72a39cb931de36b8c2a75696aa75fa5ee8b3b2
appledrft2 <ragiedoromal06@gmail.com>
2019-07-08 13:37:40 +0800

    change title

--- app/Http/Controllers/PreventiveController.php
+++ app/Http/Controllers/PreventiveController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class PreventiveController extends Controller
 {
-    public $title = "Preventive";
+    public $title = "Patient";
     /**
      * Display a listing of the resource.
      *
