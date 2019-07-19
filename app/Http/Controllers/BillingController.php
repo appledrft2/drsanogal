57864729a6a3a39f6bfff438a65aa7318729e4ab
appledrft2 <ragiedoromal06@gmail.com>
2019-07-19 14:43:27 +0800

    billing not deducting prod quantity fix

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -85,6 +85,7 @@ class BillingController extends Controller
 
 			foreach (request()->hidden_id as $key => $value) {
 				$product = Product::findOrfail(request()->hidden_id[$key]);
+                $product->quantity = $product->quantity - request()->product_quantity[$key];
 				$billingproduct = new BillingProduct();
 				$billingproduct->billing_id = $billing_id->id;
 				$billingproduct->name = request()->hidden_prodname[$key];
